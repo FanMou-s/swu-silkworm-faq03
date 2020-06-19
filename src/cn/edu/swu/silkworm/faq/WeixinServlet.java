6df13c73e58b78a455bb575db3dd729ac7461415
wusheng <wusheng@jinlutech.cn>
2020-06-19 18:12:28 +0800

    try to recevie post data

--- src/cn/edu/swu/silkworm/faq/WeixinServlet.java
+++ src/cn/edu/swu/silkworm/faq/WeixinServlet.java
@@ -4,33 +4,44 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.Enumeration;
 
 public class WeixinServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-       this.doPost(request, response);
+        String signature = request.getParameter("signature");
+        String timestamp = request.getParameter("timestamp");
+        String nonce     = request.getParameter("nonce");
+        String echoStr   = request.getParameter("echostr");
+
+        if (WeixinService.checkSignature(timestamp, nonce, signature)) {
+            PrintWriter out = response.getWriter();
+            out.print(echoStr);
+            out.flush();
+            out.close();
+            System.out.println("微信接入成功!");
+        }else {
+            System.out.println("微信接入失败!");
+        }
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String signature = request.getParameter("signature");
         String timestamp = request.getParameter("timestamp");
         String nonce = request.getParameter("nonce");
-        String echoStr = request.getParameter("echostr");
-        System.err.println(signature);
-        System.err.println(timestamp);
-        System.err.println(nonce);
-        System.err.println(echoStr);
-        if(WeixinService.checkSignature(timestamp, nonce, signature)) {
-            PrintWriter out = response.getWriter();
-            out.print(echoStr);
-            out.flush();
-            out.close();
-            System.out.println("接入成功!");
-        }else {
-            System.out.println("接入失败!");
+
+//        InputStream inputStream = request.getInputStream();
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+//        reader.readLine();
+        for (Enumeration<String> names = request.getParameterNames(); names.hasMoreElements(); ) {
+            String name = names.nextElement();
+            System.out.println("name:" + name);
+            System.out.println(request.getParameter(name));
         }
+
     }
+
+
 
 }
